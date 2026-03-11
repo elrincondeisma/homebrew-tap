4ee7bdb95c554a82cb32925b297dc4bc19ab6c94
Ismael Catala Gil <ismael.catala@smileandlearn.com>
2026-03-11 21:05:27 +0100

    fix: auto-remove quarantine attribute for unsigned app

--- Casks/keypet.rb
+++ Casks/keypet.rb
@@ -2,7 +2,8 @@ cask "keypet" do
   version "1.0.2"
   sha256 "be60890f04216237bc4d47777b1a5333984a4962addd77ff1cac529be8f19c77"
 
-  url "https://github.com/elrincondeisma/keypet/releases/download/v#{version}/KeyPet-#{version}-arm64.dmg"
+  url "https://github.com/elrincondeisma/keypet/releases/download/v#{version}/KeyPet-#{version}-arm64.dmg",
+      verified: "github.com/elrincondeisma/keypet/"
   name "KeyPet"
   desc "Desktop pixel pet that reacts to your keyboard activity"
   homepage "https://github.com/elrincondeisma/keypet"
@@ -10,6 +11,12 @@ cask "keypet" do
   depends_on arch: :arm64
 
   app "KeyPet.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{appdir}/KeyPet.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/keypet",
